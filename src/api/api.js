da7e4dfadad1206f38d70a47cdda012dec375308
Mohit-debug <71378674+Mohit-debug@users.noreply.github.com>
2024-10-30 14:02:59 +0530

    Update api.js
    'https://gpumarketplaceserver-production.up.railway.app/api',

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://gpu-marketplace-server-mohits-projects-032640aa.vercel.app/',
+  baseURL: 'https://gpumarketplaceserver-production.up.railway.app/api',
 });
 
 export const setAuthToken = (token) => {
