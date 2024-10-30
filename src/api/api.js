a40f9989d215b080ed345d5bd6092c3f6d40fe31
Mohit-debug <71378674+Mohit-debug@users.noreply.github.com>
2024-10-30 12:50:44 +0530

    Update api.js
    'https://gpumarketplaceserver-production.up.railway.app/api',

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: 'https://gpumarketplaceserver-production.up.railway.app/api',
 });
 
 export const setAuthToken = (token) => {
